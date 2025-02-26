65d1797112c906f1b9858120b92eeeee6d89a894
tungduong150105 <tungduong150105@gmail.com>
2025-02-26 22:25:15 +0700

    Change database to Postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2025_02_26_131815) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bands", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -19,7 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_02_26_131815) do
 
   create_table "members", force: :cascade do |t|
     t.string "name"
-    t.integer "band_id", null: false
+    t.bigint "band_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["band_id"], name: "index_members_on_band_id"
